98bd0c20bb97dd0132b6895fc719d9511f714b90
skiloop <skiloop@gmail.com>
2018-06-09 10:55:13 +0800

    allow copy and truncate together

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ var (
 	input    = flag.String("i", "", "input filename")
 	output   = flag.String("o", "", "destination filename")
 	position = flag.Int64("p", 0, "start position to copy")
-	whence   = flag.Int("w", 0, "according to whence: 0 means relative to the origin of the file, 1 means"+
+	whence   = flag.Int("w", 0, "according to whence: 0 means relative to the origin of the file, 1 means "+
 		"relative to the current offset, and 2 means relative to the end.")
 	bufSize  = flag.Uint("b", 1024, "buffer size")
 	truncate = flag.Bool("t", false, "truncate file")
@@ -19,15 +19,17 @@ var (
 func main() {
 	flag.Parse()
 
-	if *truncate {
-		err := fsissors.FileTruncate(*input, *position)
-		if err != nil {
-			fmt.Printf("failed to truncate file: %s", err.Error())
-		}
-	} else {
+	if *output != "" {
 		err := fsissors.FileTailCopy(*input, *position, *output, *whence, *bufSize)
 		if err != nil {
 			fmt.Printf("file copy error: %s\n", err.Error())
 		}
 	}
+	if *truncate {
+		err := fsissors.FileTruncate(*input, *position)
+		if err != nil {
+			fmt.Printf("failed to truncate file: %s", err.Error())
+		}
+	}
+}
 }
