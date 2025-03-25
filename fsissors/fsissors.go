7b55c8d3c267c0973bf33cdd7cee262f71cfd743
skiloop <skiloop@gmail.com>
2025-03-25 18:06:51 +0800

    feat: add FileCopy and Copy functions with detailed documentation

--- fsissors/fsissors.go
+++ fsissors/fsissors.go
@@ -6,6 +6,13 @@ import (
 	"os"
 )
 
+// FileCopy 从源文件指定位置复制指定大小的数据到目标文件
+// fileName: 源文件名
+// pos: 起始位置
+// fileOut: 目标文件名
+// whence: 位置基准，可以是 io.SeekStart, io.SeekCurrent, io.SeekEnd
+// bufSize: 缓冲区大小，如果为0则使用默认值1024
+// size: 要复制的字节数，如果为0则复制到文件末尾
 func FileCopy(fileName string, pos int64, fileOut string, whence int, bufSize uint, size int64) error {
 	if bufSize == 0 {
 		bufSize = 1024
@@ -51,35 +58,24 @@ func truncateFile(fin *os.File, pos int64) error {
 	_ = fin.Sync()
 	return nil
 }
+// Copy copies data from the reader to the writer using a buffer of the specified size.
+// If size is greater than 0, it copies exactly that many bytes.
+// If size is 0 or less, it copies until EOF.
 func Copy(reader io.Reader, writer io.Writer, bufSize uint, size int64) (err error) {
-	var n int
-	var copySize int64
-	copySize = 0
-	buf := make([]byte, bufSize)
-	for {
-		n, err = reader.Read(buf)
-
-		if err != nil && err != io.EOF {
-			return err
-		}
-		if n > 0 {
-			if size > 0 && copySize+int64(n) > size {
-				n = int(size - copySize)
-			}
-			_, writeErr := writer.Write(buf[:n])
-			if writeErr != nil {
-				return writeErr
-			}
-			copySize += int64(n)
-		}
-		if err == io.EOF || (size > 0 && copySize >= size) {
-			break
-		}
+	if size > 0 {
+		// Copy exactly 'size' bytes from reader to writer
+		_, err = io.CopyN(writer, reader, size)
+	} else {
+		// Copy until EOF
+		_, err = io.Copy(writer, reader)
 	}
-	return nil
+	return err
 }
 
-// FileTruncate truncate file
+// FileTruncate truncates the file to the specified size.
+// If the size is negative, it does nothing and returns nil.
+// If the size is greater than or equal to the current file size, it does nothing and returns nil.
+// Otherwise, it truncates the file to the specified size.
 func FileTruncate(filename string, size int64) error {
 	if size < 0 {
 		if Verbose {
